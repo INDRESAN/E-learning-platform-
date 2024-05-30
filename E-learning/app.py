3c02c84974b7ce3ba656e9063612012b80ba7de6
unknown <adithya14255@gmail.com>
2024-05-31 01:24:05 +0530

    adi

--- E-learning/app.py
+++ E-learning/app.py
@@ -14,6 +14,118 @@ class SQLAlchemy(_BaseSQLAlchemy):
         options["pool_pre_ping"] = True
 
 # run database
+questions=[
+    {
+        "id": 1,
+        "question": "What is the primary purpose of programming languages?",
+        "options": [
+            "To enable computers to understand human emotions",
+            "To enable humans to communicate instructions to computers",
+            "To enhance graphics and sound in video games",
+            "To design computer hardware"
+        ],
+        "answer": 1
+    },
+    {
+        "id": 2,
+        "question": "Which language was the earliest form of programming used to communicate directly with a computer's hardware?",
+        "options": [
+            "Assembly Language",
+            "High-Level Language",
+            "Machine Language",
+            "Fourth-Generation Language"
+        ],
+        "answer": 2
+    },
+    {
+        "id": 3,
+        "question": "What is an example of a high-level programming language developed for scientific computing in 1957?",
+        "options": [
+            "COBOL",
+            "FORTRAN",
+            "Python",
+            "JavaScript"
+        ],
+        "answer": 1
+    },
+    {
+        "id": 4,
+        "question": "Which type of programming language emerged in the 1970s and 1980s and is designed to be closer to natural human language?",
+        "options": [
+            "Machine Language",
+            "Assembly Language",
+            "Fourth-Generation Languages",
+            "Low-Level Languages"
+        ],
+        "answer": 2
+    },
+    {
+        "id": 5,
+        "question": "Which programming paradigm is based on the concept of objects, which are instances of classes encapsulating data and behavior?",
+        "options": [
+            "Procedural Languages",
+            "Functional Languages",
+            "Object-Oriented Languages",
+            "Scripting Languages"
+        ],
+        "answer": 2
+    },
+    {
+        "id": 6,
+        "question": "Which of the following is NOT a component of programming languages?",
+        "options": [
+            "Syntax",
+            "Semantics",
+            "Compilers",
+            "Control Structures"
+        ],
+        "answer": 2
+    },
+    {
+        "id": 7,
+        "question": "Which language is known for its high-level, interpreted nature and is extensively used in data analysis and machine learning?",
+        "options": [
+            "C++",
+            "JavaScript",
+            "Python",
+            "Ruby"
+        ],
+        "answer": 2
+    },
+    {
+        "id": 8,
+        "question": "Which language was designed by Apple for performance and safety, specifically for iOS and macOS app development?",
+        "options": [
+            "C#",
+            "Swift",
+            "Ruby",
+            "Kotlin"
+        ],
+        "answer": 1
+    },
+    {
+        "id": 9,
+        "question": "What is a trend in programming languages that focuses on supporting multiple programming paradigms for greater flexibility?",
+        "options": [
+            "Development of Quantum Computing Languages",
+            "Rise of Multi-Paradigm Languages",
+            "Improved Performance and Efficiency",
+            "Low-Code and No-Code Platforms"
+        ],
+        "answer": 1
+    },
+    {
+        "id": 10,
+        "question": "Which language is gaining traction due to its emphasis on memory safety and prevention of common programming errors that lead to security vulnerabilities?",
+        "options": [
+            "Rust",
+            "C",
+            "Prolog",
+            "SQL"
+        ],
+        "answer": 0
+    }
+]
 
 db = SQLAlchemy(app)
 
@@ -21,7 +133,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    title = db.Column(db.String(120), nullable=False)
+    title = db.Column(db.String(120))
     grades = db.Column(db.String(120), nullable=False)
 
 with app.app_context():
@@ -49,7 +161,7 @@ def signup():
     if request.method=="POST":
         uname = request.form["uname"]
         pwd = request.form["pswd"]
-        user = User(password=pwd, fullname=uname)
+        user = User(password=pwd, fullname=uname,grades=0)
         db.session.add(user)
         db.session.commit()
         if user:
@@ -65,113 +177,18 @@ def home():
        db.session.commit()
     return render_template("home.html",user=session['uname'])
 
-questions = [
-    {
-        "question": "What is the primary purpose of programming languages?",
-        "options": [
-            "To enable computers to understand human emotions",
-            "To enable humans to communicate instructions to computers",
-            "To enhance graphics and sound in video games",
-            "To design computer hardware"
-        ],
-        "answer": 1
-    },
-    {
-        "question": "Which language was the earliest form of programming used to communicate directly with a computer's hardware?",
-        "options": [
-            "Assembly Language",
-            "High-Level Language",
-            "Machine Language",
-            "Fourth-Generation Language"
-        ],
-        "answer": 2
-    },
-    {
-        "question": "What is an example of a high-level programming language developed for scientific computing in 1957?",
-        "options": [
-            "COBOL",
-            "FORTRAN",
-            "Python",
-            "JavaScript"
-        ],
-        "answer": 1
-    },
-    {
-        "question": "Which type of programming language emerged in the 1970s and 1980s and is designed to be closer to natural human language?",
-        "options": [
-            "Machine Language",
-            "Assembly Language",
-            "Fourth-Generation Languages",
-            "Low-Level Languages"
-        ],
-        "answer": 2
-    },
-    {
-        "question": "Which programming paradigm is based on the concept of objects, which are instances of classes encapsulating data and behavior?",
-        "options": [
-            "Procedural Languages",
-            "Functional Languages",
-            "Object-Oriented Languages",
-            "Scripting Languages"
-        ],
-        "answer": 2
-    },
-    {
-        "question": "Which of the following is NOT a component of programming languages?",
-        "options": [
-            "Syntax",
-            "Semantics",
-            "Compilers",
-            "Control Structures"
-        ],
-        "answer": 2
-    },
-    {
-        "question": "Which language is known for its high-level, interpreted nature and is extensively used in data analysis and machine learning?",
-        "options": [
-            "C++",
-            "JavaScript",
-            "Python",
-            "Ruby"
-        ],
-        "answer": 2
-    },
-    {
-        "question": "Which language was designed by Apple for performance and safety, specifically for iOS and macOS app development?",
-        "options": [
-            "C#",
-            "Swift",
-            "Ruby",
-            "Kotlin"
-        ],
-        "answer": 1
-    },
-    {
-        "question": "What is a trend in programming languages that focuses on supporting multiple programming paradigms for greater flexibility?",
-        "options": [
-            "Development of Quantum Computing Languages",
-            "Rise of Multi-Paradigm Languages",
-            "Improved Performance and Efficiency",
-            "Low-Code and No-Code Platforms"
-        ],
-        "answer": 1
-    },
-    {
-        "question": "Which language is gaining traction due to its emphasis on memory safety and prevention of common programming errors that lead to security vulnerabilities?",
-        "options": [
-            "Rust",
-            "C",
-            "Prolog",
-            "SQL"
-        ],
-        "answer": 0
-    }
-]
+
 @app.route('/course',methods=['POST','GET'])
 def course():
     if request.method=='POST':
        db.session.commit()
-    return render_template("quizzes.html")
+    return render_template("courses.html")
+
+@app.route('/course_details',methods=['POST','GET'])
+def course_details_1():
+    if request.method=='POST':
+       db.session.commit()
+    return render_template("course_details1.html")
 
 @app.route('/quiz')
 def quiz():
@@ -179,16 +196,21 @@ def quiz():
        db.session.commit()
     return render_template('quizzes.html', questions=questions)
 
-@app.route('/submit', methods=['POST'])
+
+
+@app.route('/submit', methods=['POST','GET'])
 def submit():
-    score = 0
-    for i, question in enumerate(questions):
-        answer = int(request.form.get(f'question{i}'))
-        if answer == question['answer']:
-            score += 1
-    return render_template('result.html', score=score, total=len(questions))
+    # if request.method=='POST':
+    #     score = 0
+    #     print(request.form.getlist('a'))
+    #     for question in questions:
+    #         answer = int(request.form.get(f'question{i}'))
+    #         if answer == question['answer']:
+    #             score += 1
+    return render_template('result.html', score=6, total=len(questions))
+
 @app.route('/mycourse',methods=['POST','GET'])
-def home():
+def mycourse():
     if request.method=='POST':
        db.session.commit()
     return render_template("home.html",user=session['uname'])
@@ -196,6 +218,8 @@ def home():
 def result():
     if request.method=='POST':
        db.session.commit()
-    return render_template('home.html')
+    return render_template('result.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
